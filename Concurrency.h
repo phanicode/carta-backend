609fd477d8bdd1f6d7905d3f7c360021ae956ade
Rob Simmonds <simmonds@cs.uct.ac.za>
2020-01-09 09:49:29 +0200

    Prevent mutexes being unlocked twice.

--- Concurrency.h
+++ Concurrency.h
@@ -1,6 +1,11 @@
+#pragma once
+
 #include <iostream>
 #include <mutex>
+#include <shared_mutex>
 #include <list>
+
+#define __USE_TBB__ 0
 
 template <class T>
 class concurrent_queue {
@@ -36,3 +41,89 @@ private:
     std::list<T> _q;
     std::mutex _mtx;
 };
+
+
+#if __USE_TBB__
+#include <tbb/queuing_rw_mutex.h>
+
+class rw_mutex_writer {
+ public:
+	rw_mutex_writer(tbb::queuing_rw_mutex& mtx) {
+		_lock.acquire(mtx,true);
+	}
+	~rw_mutex_writer() {
+	}
+	void release() {
+		_lock.release();
+	}
+ private:
+	tbb::queuing_rw_mutex::scoped_lock _lock;
+};
+
+class rw_mutex_reader {
+ public:
+	rw_mutex_reader(tbb::queuing_rw_mutex& mtx) {
+		_lock.acquire(mtx,false);
+	}
+	~rw_mutex_reader() {
+	}
+	void release() {
+		_lock.release();
+	}
+ private:
+	tbb::queuing_rw_mutex::scoped_lock _lock;
+};
+#else
+class rw_mutex_writer {
+ public:
+	rw_mutex_writer(std::shared_mutex& mtx) {
+	   	_mtx = &mtx;
+		//		std::cout << this << " " << &mtx  << " rw_mutex_writer "  << std::endl;
+		mtx.lock();
+		is_locked = true;
+	}
+	~rw_mutex_writer() {
+		//		std::cout << this << " " << _mtx  << " ~rw_mutex_writer "  << std::endl;
+		if(is_locked) {
+			_mtx->unlock();
+		}
+	}
+	void release() {
+		//		std::cout << this << " " << _mtx  << " rw_mutex_writer::release "  << std::endl;
+		if(is_locked) {
+			is_locked = false;
+			_mtx->unlock();
+		}
+	}
+ private:
+	std::shared_mutex *_mtx;
+		bool is_locked;
+};
+
+class rw_mutex_reader {
+ public:
+	rw_mutex_reader(std::shared_mutex& mtx) {
+		//		std::cout << this << " " << &mtx  << " rw_mutex_reader "  << std::endl;
+		mtx.lock_shared();
+		_mtx = &mtx;
+		is_locked = true;
+	}
+	~rw_mutex_reader() {
+		//		std::cout << this << " " << _mtx  << " ~rw_mutex_reader "  << std::endl;
+		if(is_locked) {
+			_mtx->unlock_shared();
+		}
+		
+	}
+	void release() {
+		//		std::cout  << this << " " << _mtx << " rw_mutex_reader::release "<< std::endl;
+		if(is_locked) {
+			is_locked = false;
+			_mtx->unlock_shared();
+		}
+	}
+ private:
+	std::shared_mutex * _mtx;
+	bool is_locked;
+};
+#endif
